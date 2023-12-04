209dd290f26501b476e052cccb0ce3a031d94810
TomasRamalho <149377449+TomasRamalho@users.noreply.github.com>
2023-12-04 11:22:44 +0000

    Update PP.py

--- PP.py
+++ PP.py
@@ -1,16 +1,35 @@
 #Construtor
-def __init__(self, nome, interesses, artigos_disponiveis)
+def __init__(self, nome, interesses, artigos_disponiveis):
 #Altera os interesses e/ou os artigos de um utilizador
-def editar_conta(self, novos_interesses, novos_artigos)
+def editar_conta(self, novos_interesses, novos_artigos):
 #Adiciona uma nova avaliação, podendo incluir um comentário
-def deixar_avaliacao(self, estrelas, comentario)
+def deixar_avaliacao(self, estrelas, comentario):
 #Apresenta todas as avaliações e comentários
-def listar_avaliacoes(self)
+def listar_avaliacoes(self):
 #Apresenta todos os interesses
-def mostrar_interesses(self)
+def mostrar_interesses(self):
 #Apresenta todos os artigos
-def mostrar_artigos(self)
+def mostrar_artigos(self):
 #Altera o número de pycoins
-def alterar_pycoins(self, numero_pycoins)
+def alterar_pycoins(self, numero_pycoins):
 #Apresenta o número de pycoins
-def mostrar_pycoins(self)
+def mostrar_pycoins(self):
+
+#Construtor
+def __init__(self, nome, preco, tipologia, quantidade):
+#Altera o nome de um artigo para o novo nome recebido
+def editar_nome(self, nome):
+#Altera o preço de um artigo de acordo com a percentagem dada
+def ajustar_preco(self, percentagem_alteracao):
+#Altera o preço para o novo preço recebido
+def editar_preco(self, preco):
+#Apresenta o preço do artigo
+def mostrar_preco(self):
+#Altera a quantidade
+def editar_quantidade(self, nova_quantidade):
+#Apresenta a quantidade do artigo
+def mostrar_quantidade(self):
+#Altera a tipologia
+def editar_tipo (self, novo_tipo):
+#Apresenta a tipologia do artigo
+def mostrar_tipo (self):

e53d458310071badf7e9e22f5a6b8667b069c0b9
TomasRamalho <149377449+TomasRamalho@users.noreply.github.com>
2023-12-27 16:33:18 +0000

    Update PP.py
    main

--- PP.py
+++ PP.py
@@ -20,6 +20,7 @@ def mostrar_interesses(self):
     print(f"Interesses de {self.mostrar_insteresses}: {', '.join(self.interesses)}")
 #Apresenta todos os artigos
 def mostrar_artigos(self):
+    
 #Altera o número de pycoins
 def alterar_pycoins(self, numero_pycoins):
     self.numero_pycoins = numero_pycoins
@@ -88,8 +89,6 @@ def exportar_artigos_preco(self, nome_ficheiro):
 def exportar_utilizadores(self):
 #Início da feira. O grupo deve apresentar testes do projeto nesta função
 
-def main():
-
 #Construtor
 def __init__(self):
 #Adiciona um novo artigo
@@ -101,3 +100,70 @@ def remover_artigo(self, artigo):
 #Mostra o nome, preço e quantidade do artigo recebido
 def mostrar_artigo(self, artigo):
     mostrar_artigo = artigo
+
+def main():
+    FeiraVirtual = FeiraVirtual()
+    user = Utilizador(input[""], [""], [""])
+    print("Bem-vindo/a à Feira Virtual. Pretende aceder a:\1 - Utilizadores\2 - Artigos\3 - Mercado")
+    opcao1 = int(input())
+    if opcao1 == 1:
+        print("Pretende aceder a:\n1 – Registo de utilizadores\n2 – Alteração de um utilizador\n3 – Eliminação de conta de um utilizador\n4 – Lista de utilizadores\n5 – Mostrar artigos de um utilizador\n6 – Mostrar interesses de um utilizador\n7 – Mostrar Pycoins de um utilizador\nV – Voltar atrás\nS – Sair")
+        opcao11 = input("")
+        if opcao11 == 1:
+            print("1 – Registo manual\n2 – Registo por ficheiro")
+            opcao12 = input("")
+            if opcao12 == 1:
+            if opcao12 == 2:
+            while opcao12 < 1 or opcao12 > 2:
+                opcao12 = input("")
+        if opcao11 == 2:
+        if opcao11 == 3:
+        if opcao11 == 4:
+        if opcao11 == 5:
+            print("Introduza um utilizador para consultar os seus artigos:")
+            opcao15 = input("")
+            print("Artigo: n, Preço: n, Quantidade: n")
+        if opcao11 == 6:
+            for utilizador in self.nome:
+            utilizador.mostrar_interesses()
+        if opcao11 == 7:
+        if opcao21 == "S":
+            print("Bem-vindo/a à Feira Virtual. Pretende aceder a:\1 - Utilizadores\2 - Artigos\3 - Mercado")
+            opcao1 = input("")
+        if opcao21 == "V":
+            quit()
+
+    if opcao1 == 2:
+        print("Pretende aceder a: \n1 – Mostrar preço de um artigo\n2 – Mostrar quantidade de um artigo\n3 – Mostrar tipo de um artigo\nV – Voltar atrás\nS – Sair")
+        opcao21 = input("")
+        if opcao21 == 1:
+        if opcao21 == 2:
+        if opcao21 == 3:
+        if opcao21 == "S":
+            print("Bem-vindo/a à Feira Virtual. Pretende aceder a:\1 - Utilizadores\2 - Artigos\3 - Mercado")
+            opcao1 = input("")
+        if opcao21 == "V":
+            quit()
+            
+    if opcao1 == 3:
+        print("Pretende aceder a:\n1 – Adicionar Artigo ao Mercado\n2 – Remover Artigo do Mercado\n3 – Listar Artigos do Mercado\nV – Voltar atrás\nS – Sair")
+        opcao31 = input("")
+        if opcao31 == 1:
+            print("Insira os detalhes do artigo para adicionar ao mercado (nome, tipologia, preço,quantidade):")
+            opcao311 = input("")
+        if opcao31 == 2:
+        if opcao31 == 3:
+            print("Artigos Disponíveis no Mercado:\n1. xxx \n2. xxx \n3. xxx")
+        if opcao31 == "S":
+            print("Bem-vindo/a à Feira Virtual. Pretende aceder a:\1 - Utilizadores\2 - Artigos\3 - Mercado")
+            opcao1 = input("")
+        if opcao31 == "V":
+            quit()
+
+    else:
+        print("Bem-vindo/a à Feira Virtual. Pretende aceder a:\1 - Utilizadores\2 - Artigos\3 - Mercado")
+        opcao1 = input("")
+    
+    
+if __name__=="__main__":
+    main()
